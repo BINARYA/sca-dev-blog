9a38ea07facf8ffff5cd77805bcf0d1f7fd974b6
mrjungle <davide.nuccio@gmail.com>
2025-09-18 02:35:31 +0200

    20250918_0

--- pelicanconf.py
+++ pelicanconf.py
@@ -61,5 +61,5 @@ EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'},
 }
 
-OUTPUT_PATH = ""
+OUTPUT_PATH = "docs/"
 
--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://binarya.github.io/sca-dev-blog/"
+SITEURL = "https://binarya.github.io/sca-dev-blog"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
